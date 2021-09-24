d04247d1a707fb3c0eb349d6d8d7b439b47bbf79
pozniako16 <lev.pozniakoff@gmail.com>
2021-09-24 14:04:59 +0200

    ajout image d'internet

--- app/src/main/java/ch/heigvd/iict/sym/labo1/SecondActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/SecondActivity.java
@@ -2,8 +2,11 @@ package ch.heigvd.iict.sym.labo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import ch.heigvd.iict.sym.labo1.network.ImageDownloader;
 
 import org.w3c.dom.Text;
 
@@ -16,12 +19,11 @@ public class SecondActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String email = extras.getString("account");
         TextView accountView = (TextView) findViewById(R.id.account_text);
-        if (extras != null) {
-            accountView.setText(email);
-            //The key argument here must match that used in the other activity
-        } else {
-            accountView.setText("Hello");
-        }
+        accountView.setText(email);
+        //The key argument here must match that used in the other activity
+        ImageView connected_image = (ImageView) findViewById(R.id.connected_image);
+        new ImageDownloader(connected_image, "https://thispersondoesnotexist.com/image").show();
+
 
     }
 }
